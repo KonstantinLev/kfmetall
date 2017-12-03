3a13d60e20d66bbbdfbf418f7cd75f9cb506cd82
Konstantin <x-stels@bk.ru>
2017-12-03 17:30:39 +0300

    правки в стилях + реализован каталог

---
+++ catalog.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#4e93bc">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#4e93bc">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#4e93bc">
+
+    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+
+    <link rel="stylesheet" href="css/jquery.mmenu.all.css" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/hamburgers.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/media.css">
+    <title>KFMetall</title>
+</head>
+<body>
+
+<div id="my-page">
+    <div id="my-header">
+        <?php include_once ('parts/header.html')?>
+    </div>
+    <div id="my-content">
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="block-catalog">
+                        <div class="col-md-3">
+                            <div class="left-menu">
+                                <ul>
+                                    <li>
+                                        <span>Профиль перфорированный</span>
+                                        <ul>
+                                            <li><a href="#">ПУ 20</a></li>
+                                            <li><a href="#">ПУ 25</a></li>
+                                            <li><a href="#">ПМ 23*6</a></li>
+                                            <li><a href="#">ПМ 23*-10</a></li>
+                                            <li><a href="#">ПУ 35*35</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <span>Профиль для монтажа гипосокартона</span>
+                                        <ul>
+                                            <li><a href="#">ПП 60*27</a></li>
+                                            <li><a href="#">ПН 27*28</a></li>
+                                            <li><a href="#">ПН 50*40</a></li>
+                                            <li><a href="#">ПС 50*50</a></li>
+                                            <li><a href="#">ПН 75*40</a></li>
+                                            <li><a href="#">ПС 75*50</a></li>
+                                            <li><a href="#">ПН 100*40</a></li>
+                                            <li><a href="#">ПС 100*50</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <span>Крепежные изделия</span>
+                                        <ul>
+                                            <li><a href="#">Лента монтажная 12*25</a></li>
+                                            <li><a href="#">Лента монтажная 20*25</a></li>
+                                            <li><a href="#">KU 90*90*65*2.0mm</a></li>
+                                            <li><a href="#">KUU 70*70*55*2.0mm</a></li>
+                                            <li><a href="#">KUR 40*40*40*2.0mm</a></li>
+                                            <li><a href="#">KUR 60*60*60*2.0mm</a></li>
+                                            <li><a href="#">KUU 105*105*105*2.0mm</a></li>
+                                            <li><a href="#">KUU 50*50*35*2.0mm</a></li>
+                                            <li><a href="#">KUU 90*90*40*2.0mm</a></li>
+                                            <li><a href="#">PS 40*160*2.0mm</a></li>
+                                            <li><a href="#">PS 80*240*2.0mm</a></li>
+                                            <li><a href="#">PS 60*300*2.0mm</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <span>Комплектующие</span>
+                                        <ul>
+                                            <li><a href="#">Анкерный подвес с зажимом</a></li>
+                                            <li><a href="#">Тяга</a></li>
+                                            <li><a href="#">Соединитель одноуровневый "краб"</a></li>
+                                            <li><a href="#">Пружинный подвес</a></li>
+                                            <li><a href="#">Соединитель 2-х уровневый</a></li>
+                                            <li><a href="#">Подвес прямой</a></li>
+                                            <li><a href="#">Удлинитель ПП-профиля</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="row">
+                                <ol class="breadcrumb">
+                                    <li><a href="#">Главная</a></li>
+                                    <li class="active">Каталог</li>
+                                </ol>
+                                <div class="col-md-6">
+                                    <div class="block-product">
+                                        <a href="profile.php" title="Профиль перфорированный">
+                                            <img src="img/catalog/profile.jpg" alt="Профиль перфорированный">
+                                            <span>Профиль перфорированный</span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="block-product">
+                                        <a href="#" title="Профиль для монтажа гипсокартона">
+                                            <img src="img/catalog/profile-gypsum.jpg" alt="Профиль для монтажа гипсокартона">
+                                            <span>Профиль для монтажа гипсокартона</span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="block-product">
+                                        <a href="#" title="Крепежные изделия">
+                                            <img src="img/catalog/fixture.jpg" alt="Крепежные изделия">
+                                            <span>Крепежные изделия</span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="block-product">
+                                        <a href="#" title="Комплектующие">
+                                            <img src="img/catalog/complect.jpg" alt="Комплектующие">
+                                            <span>Комплектующие</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="my-footer">
+        <footer>
+
+        </footer>
+        <!--Модалка-->
+        <?php include_once ('parts/modal.html')?>
+    </div>
+</div>
+
+
+
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.mmenu.all.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -16,4 +16,12 @@ $(document).ready(function() {
     }).bind('close:finish', function(){
         $('.hamburger').removeClass('is-active');
     });
+
+    $('.top-menu a').each(function () {
+        var location = window.location.href;
+        var link = this.href;
+        if(location == link) {
+            $(this).addClass('active');
+        }
+    });
 });
